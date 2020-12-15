cfc7cdeb1fffcd59f2b268ed674f2e42623be62a
Rachel Wagschal <rawagschal@gmail.com>
2020-12-14 23:58:53 -0800

    stuck at product-route get all

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,16 +7,30 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  // Product.findAll({
-  //   attributes: [
-  //     'id',
-  //     'product_name',
-  //     'price',
-  //     'stock',
-  //     'category_id',
-  //     'tag_name'
-  //   ] 
-  // })
+  Product.findAll({
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+      'category_id',
+    ],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name'],
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+      }
+    ] 
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+  });
 });
 
 // get one product
@@ -27,15 +41,13 @@ router.get('/:id', (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
+  Product.create({
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      tagIds: [req.body.tag_id]
+  })
+  // Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
