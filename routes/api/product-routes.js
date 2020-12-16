b3239758eadfbd159e617dce73d58d44279e5d92
Rachel Wagschal <rawagschal@gmail.com>
2020-12-15 16:33:01 -0800

    fixed products delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -114,6 +114,19 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((products) => {
+    console.log(products);
+    res.json(products);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 module.exports = router;
